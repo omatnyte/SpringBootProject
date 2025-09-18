34994a9b827fc83cc2b71200bd4f6c533264d4fc
omatnyte <omatnyte@gmail.com>
2025-09-19 05:14:34 +0530

    ResponseEntity and Lombook

---
+++ src/main/java/com/springboot/SpringBootProject/controller/JournalEntryResponseEntityController.java
@@ -0,0 +1,82 @@
+package com.springboot.SpringBootProject.controller;
+
+
+import com.springboot.SpringBootProject.entity.JournalEntry;
+import com.springboot.SpringBootProject.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api-2/journal")
+public class JournalEntryResponseEntityController {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+
+
+    @GetMapping()
+    public List<JournalEntry> getAllEntries() {
+
+        return journalEntryService.getAllEntries();
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<?> getEntryById(@PathVariable ObjectId id) {
+        Optional<JournalEntry> entry = journalEntryService.getEntryById(id);
+
+        if(entry.isPresent()){
+            System.out.println("Hello");
+            return new ResponseEntity<>(entry.get(), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Journal entry not found", HttpStatus.NOT_FOUND);
+        }
+
+
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createEntry(@RequestBody JournalEntry entry) {
+
+        try{
+            entry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(entry);
+            return new ResponseEntity<>("Journal entry added successfully", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error creating journal entry: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/id/{id}")
+    public ResponseEntity<?> deleteEntryById(@PathVariable ObjectId id) {
+        try {
+            journalEntryService.deleteEntryById(id);
+            return new ResponseEntity<>("Journal entry deleted successfully", HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error deleting journal entry: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/update/id/{id}")
+    public ResponseEntity<?> updateEntryById(@PathVariable ObjectId id, @RequestBody JournalEntry updatedEntry) {
+        JournalEntry oldEntry = journalEntryService.getEntryById(id).orElse(null);
+        if (oldEntry != null) {
+            oldEntry.setTitle((updatedEntry.getTitle() == null || updatedEntry.getTitle().isEmpty()) ? oldEntry.getTitle() : updatedEntry.getTitle());
+            oldEntry.setContent((updatedEntry.getContent() == null || updatedEntry.getContent().isEmpty()) ? oldEntry.getContent() : updatedEntry.getContent());
+            oldEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(oldEntry);
+            return new ResponseEntity<>("Journal entry updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Journal entry not found", HttpStatus.NOT_FOUND);
+        }
+    }
+}
--- src/main/java/com/springboot/SpringBootProject/entity/JournalEntry.java
+++ src/main/java/com/springboot/SpringBootProject/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.springboot.SpringBootProject.entity;
 
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,6 +8,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection="journal_entries")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+//@Data
 public class JournalEntry {
 
     //Changing dataType from String to ObjectId type for mongodb Id
@@ -17,50 +25,50 @@ public class JournalEntry {
 
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public JournalEntry(ObjectId id, String title, String content, LocalDateTime date) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.date = date;
-    }
-
-    public JournalEntry() {
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "JournalEntry [id=" + id + ", title=" + title + ", content=" + content + ", date=" + date + "]";
-    }
+//    public LocalDateTime getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDateTime date) {
+//        this.date = date;
+//    }
+//
+//    public JournalEntry(ObjectId id, String title, String content, LocalDateTime date) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//        this.date = date;
+//    }
+//
+//    public JournalEntry() {
+//    }
+//
+//    public ObjectId getId() {
+//        return id;
+//    }
+//
+//    public void setId(ObjectId id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "JournalEntry [id=" + id + ", title=" + title + ", content=" + content + ", date=" + date + "]";
+//    }
 }
